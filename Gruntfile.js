1f69f8c9af5e790e5ede2d0fd07a9cd240c6965f
Luca Tumedei <luca@theaveragedev.com>
2014-04-17 07:09:53 +0200

    modified the file to concatenate spin.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,25 +5,21 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         concat: {
             options: {
-                stripBanners: true,
-                banner: '/*! <%= pkg.title %> - v<%= pkg.version %>\n' +
-                ' * <%= pkg.homepage %>\n' +
-                ' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
-                ' * Licensed GPLv2+' +
-                ' */\n'
+                stripBanners: true
             },
             ajax_navigation: {
                 src: [
-                'assets/js/src/ajax_navigation.js'
+                    'assets/js/vendor/spin/spin.min.js',
+                    'assets/js/src/ajax_navigation.js'
                 ],
                 dest: 'assets/js/ajax_navigation.js'
             }
         },
         jshint: {
             all: [
-            'Gruntfile.js',
-            'assets/js/src/**/*.js',
-            'assets/js/test/**/*.js'
+                'Gruntfile.js',
+                'assets/js/src/**/*.js',
+                'assets/js/test/**/*.js'
             ],
             options: {
                 curly: true,
@@ -48,22 +44,17 @@ module.exports = function(grunt) {
                     'assets/js/ajax_navigation.min.js': ['assets/js/ajax_navigation.js']
                 },
                 options: {
-                    banner: '/*! <%= pkg.title %> - v<%= pkg.version %>\n' +
-                    ' * <%= pkg.homepage %>\n' +
-                    ' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
-                    ' * Licensed GPLv2+' +
-                    ' */\n',
                     mangle: {
                         except: ['jQuery']
                     },
-                    sourceMap: true
+                    preserveComments: 'some'
                 }
             }
         },
         test: {
             files: ['assets/js/test/**/*.js']
         },
-        
+
         sass: {
             all: {
                 files: {
@@ -75,65 +66,66 @@ module.exports = function(grunt) {
         cssmin: {
             options: {
                 banner: '/*! <%= pkg.title %> - v<%= pkg.version %>\n' +
-                ' * <%= pkg.homepage %>\n' +
-                ' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
-                ' * Licensed GPLv2+' + ' */\n'},
-                minify: {
-                    expand: true,
-
-                    cwd: 'assets/css/',
-                    src: ['ajax_navigation.css'],
-                    
-                    dest: 'assets/css/',
-                    ext: '.min.css'
-                }
+                    ' * <%= pkg.homepage %>\n' +
+                    ' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
+                    ' * Licensed GPLv2+' + ' */\n'
             },
-            autoprefixer: {
-                all: {
-                    options: {
-                        browsers: ['last 2 versions']
-                    },
-                    src: 'assets/css/ajax_navigation.css',
-                    dest: 'assets/css/ajax_navigation.css'
-                }
-            },
-            watch: {
+            minify: {
+                expand: true,
 
-                sass: {
-                    files: ['assets/css/sass/*.scss'],
-                    tasks: ['sass','autoprefixer:all','cssmin'],
-                    options: {
-                        debounceDelay: 500
-                    }
+                cwd: 'assets/css/',
+                src: ['ajax_navigation.css'],
+
+                dest: 'assets/css/',
+                ext: '.min.css'
+            }
+        },
+        autoprefixer: {
+            all: {
+                options: {
+                    browsers: ['last 2 versions']
                 },
-                
-                scripts: {
-                    files: ['assets/js/src/**/*.js', 'assets/js/vendor/**/*.js'],
-                    tasks: ['jshint', 'concat', 'uglify'],
-                    options: {
-                        debounceDelay: 500
-                    }
+                src: 'assets/css/ajax_navigation.css',
+                dest: 'assets/css/ajax_navigation.css'
+            }
+        },
+        watch: {
+
+            sass: {
+                files: ['assets/css/sass/*.scss'],
+                tasks: ['sass', 'autoprefixer:all', 'cssmin'],
+                options: {
+                    debounceDelay: 500
                 }
             },
-            clean: {
-                main: ['release/<%= pkg.version %>']
-            },
-            copy: {
+
+            scripts: {
+                files: ['assets/js/src/**/*.js', 'assets/js/vendor/**/*.js'],
+                tasks: ['jshint', 'concat', 'uglify'],
+                options: {
+                    debounceDelay: 500
+                }
+            }
+        },
+        clean: {
+            main: ['release/<%= pkg.version %>']
+        },
+        copy: {
             // Copy the plugin to a versioned release directory
             main: {
                 src: [
-                '**',
-                '!node_modules/**',
-                '!release/**',
-                '!.git/**',
-                '!.sass-cache/**',
-                '!css/src/**',
-                '!js/src/**',
-                '!img/src/**',
-                '!Gruntfile.js',
-                '!package.json',
-                '!.gitignore',
-                '!.gitmodules'
+                    '**',
+                    '!node_modules/**',
+                    '!release/**',
+                    '!.git/**',
+                    '!.sass-cache/**',
+                    '!css/src/**',
+                    '!js/src/**',
+                    '!img/src/**',
+                    '!Gruntfile.js',
+                    '!package.json',
+                    '!.gitignore',
+                    '!.gitmodules'
                 ],
                 dest: 'release/<%= pkg.version %>/'
             }
@@ -156,8 +148,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin'); 
-    grunt.loadNpmTasks('grunt-contrib-sass'); 
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
@@ -165,9 +157,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-autoprefixer');
 
     // Default task.
-    
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'sass', 'autoprefixer', 'cssmin']); 
-    
+
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'sass', 'autoprefixer', 'cssmin']);
+
 
     grunt.registerTask('build', ['default', 'clean', 'copy', 'compress']);
 
