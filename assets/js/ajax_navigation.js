72f68f2b06592b1179f609d33b4564b1f743ebf2
Luca Tumedei <luca@theaveragedev.com>
2014-04-29 15:12:20 +0200

    modified the block script file to use the $.ajaxify plugin

--- assets/js/ajax_navigation.js
+++ assets/js/ajax_navigation.js
@@ -294,32 +294,8 @@
  */
 
 jQuery(document).ready(function($) {
-    // cache the content area that will be filled with content
-    // :urlInternal selector from Ben Alman's urlInternal jQuery library
-    var $contentArea = $('.theContent.block-type-content .block-content'),
-        $anchors = $('.menu-ajax a:not(.openMenu,.closeMenu)').filter(':urlInternal');
-    // all the anchor tags save for the ones used in the menu
-    // should pull content via AJAX
-    $anchors.on('click', function(ev) {
-        // cache the clicked anchor
-        var $this = $(this),
-            url = '',
-            spinner = new Spinner().spin();
-        // do not follow the link
-        ev.preventDefault();
-        // get the linked url
-        url = $this.attr('href');
-        // set the spinner
-        spinner.el.style.top = '50%';
-        spinner.el.style.left = '50%';
-        // fade the content area and append the spinner in its place
-        $contentArea.fadeOut().parent().append(spinner.el);
-        // spinner = new Spinner().spin($contentArea.)
-        $contentArea.load(url + ' .theContent', function(data) {
-            // stop the spinner
-            spinner.stop();
-            // fade in the new content
-            $contentArea.fadeIn('fast');
-        });
+    $('.menu-ajax').ajaxify({
+        loadToSelector: '.theContent',
+        loadFromSelector: '.block-type-content .block-content'
     });
 });
